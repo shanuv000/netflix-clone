16962f327d8fc1eea466580ec138b7b86a4a39a6
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2021-03-04 12:21:05 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
 import './App.css';
+import LoginScreen from "./screens/LoginScreen";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
 
 function App() {
+    const user = null;
     return (
         <div className="app">
-            <HomeScreen />
+
+            <Router>
+                {!user ? (
+                    <LoginScreen/>
+                ) : (
+                    <Switch>
+                        <Route exact path="/">
+                            <HomeScreen/>
+                        </Route>
+                    </Switch>
+                )}
+
+            </Router>
         </div>
     );
 }
