e970eee689293fa479d72eaeb03591f027ebbab1
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2021-03-05 11:26:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -8,16 +8,24 @@ import {
     Route,
 } from "react-router-dom";
 import {auth} from "./firebase";
-
+import {useDispatch, useSelector} from "react-redux";
+import {login, logout, selectUser} from "./features/userSlice";
+import ProfileScreen from "./screens/ProfileScreen";
 function App() {
-    const user = null;
+    const user = useSelector(selectUser);
+    const dispatch = useDispatch();
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((userAuth) => {
             if (userAuth) {
                 //Logged In
-                console.log(userAuth);
+                dispatch(login({
+                        uid: userAuth.uid,
+                        email: userAuth.email
+                    })
+                );
             } else {
                 //Logged Out
+                dispatch(logout);
             }
         })
         return unsubscribe;
@@ -32,6 +40,9 @@ function App() {
                     <LoginScreen/>
                 ) : (
                     <Switch>
+                        <Route path="/profile">
+                            <ProfileScreen/>
+                        </Route>
                         <Route exact path="/">
                             <HomeScreen/>
                         </Route>
--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import './Nav.css';
+import {useHistory} from "react-router-dom"
 
 const Nav = () => {
     const [show, handleShow] = useState(false);
+    const history = useHistory();
+
     const transitionNavBar = () => {
         if (window.scrollY > 100) {
             handleShow(true);
@@ -19,12 +22,15 @@ const Nav = () => {
         <div className={`nav ${show && "nav__black"}`}>
             <div className="nav__contents">
                 <img className='nav__logo'
+                     onClick={()=>history.push('/')}
                      src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
                      alt="netflix-logo"
                 />
-                <img className='nav__avatar'
-                     src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png"
-                     alt="netflix-avatar"
+                <img
+                    onClick={() => history.push('/profile')}
+                    className='nav__avatar'
+                    src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png"
+                    alt="netflix-avatar"
                 />
             </div>
 
