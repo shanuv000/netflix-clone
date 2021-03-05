b347898051ca504de09c70da1a83ab633d31b33d
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2021-03-05 12:42:14 +0530

    commit

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -3,6 +3,7 @@ import './ProfileScreen.css';
 import Nav from "../Nav";
 import {useSelector} from "react-redux";
 import {selectUser} from "../features/userSlice";
+import {auth} from "../firebase";
 
 const ProfileScreen = () => {
     const user = useSelector(selectUser);
@@ -16,6 +17,9 @@ const ProfileScreen = () => {
                          alt="avatar"/>
                     <div className="profileScreen__details">
                         <h2>{user.email}</h2>
+                        <div className="profileScreen__plans">
+                            <button onClick={() => auth.signOut()} className='profileScreen__signOut'>Sign Out</button>
+                        </div>
                     </div>
                 </div>
             </div>
